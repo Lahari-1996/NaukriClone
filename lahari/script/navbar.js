b30ab70c1fc24eadd8e7b164fd1d58e02b56e2cb
Lahari-1996 <101380115+Lahari-1996@users.noreply.github.com>
2022-06-16 02:22:29 +0530

    static parts,footer and other frames added

--- lahari/script/navbar.js
+++ lahari/script/navbar.js
@@ -18,4 +18,24 @@ function navbar(){
 
 }
 
+// function footer(){
+//     return `
+//     <div id="left">
+//             <img src="https://static.naukri.com/s/4/100/i/naukri_Logo.png" alt="logo">
+//         </div>
+//         <div id="mid">
+//             <div><p>JOBS</p></div>
+//             <div><p>RECRUITERS</p></div>
+//             <div><p>COMPANIES</p></div>
+//             <div><p>TOOLS</p></div>
+//             <div><p>SERVICES</p></div>
+//             <div><p>MORE</p></div>
+//         </div>
+//         <div id="right">
+//             <div><i class="fa-regular fa-bell"></i></div>
+//             <div><p>MY NAUKRI</p></div>
+//         </div>`
+
+// }
+
 export default navbar;
