6476166bda61e2b4a59f250b4b38428bb3f3b865
karishma24-max <chauhankarishma824@gmail.com>
2022-06-19 08:57:55 +0530

    l

--- karishmaji/education.js
+++ karishmaji/education.js
@@ -1,6 +1,7 @@
 document.getElementById("button").addEventListener("click",info)
 var arr= JSON.parse(localStorage.getItem("education"))||[]
 function info(){
+     console.log(1)
 var obj={
      education:document.getElementById("education").value,
      course:document.getElementById("course").value,
@@ -8,7 +9,7 @@ var obj={
      university:document.getElementById("university").value
 }
 arr.push(obj)
-window.location.href="/login/profile.html"
+window.location.href="../karishmaji/profile.html"
 console.log(1)
 localStorage.setItem("education",JSON.stringify(arr));
 }
